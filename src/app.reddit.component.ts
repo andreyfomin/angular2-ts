0c7d66c219f8b7b349ee9e2fa8a76bfef115db6f
Andrey <fomin.andre@gmail.com>
2016-04-20 16:44:32 +0300

    add new example

--- src/app.reddit.component.ts
+++ src/app.reddit.component.ts
@@ -1,22 +1,30 @@
 import {Component} from 'angular2/core';
+import {ArticleComponent} from './app.reddit-article.component';
 
 @Component({
     selector: 'reddit',
-    template: `
-    <form class="ui large form segment">
-      <h3 class="ui header">Add a Link</h3>
-      <div class="field">
-        <label for="title">Title:</label> <input name="title" #newtitle>
-      </div>
-      <div class="field">
+    directives: [ArticleComponent],
+    template: `<form class="ui large form segment">
+    <h3 class="ui header">Add a Link</h3>
+    <div class="field">
+        <label for="title">Title:</label>
+        <input name="title" #newtitle>
+    </div>
+    <div class="field">
         <label for="link">Link:</label>
         <input name="link" #newlink>
-      </div>
-      <button (click)="addArticle(newtitle, newlink)"
-              class="ui positive right floated button">
+    </div>
+
+    <button (click)="addArticle(newtitle, newlink)"
+            class="ui positive right button">
         Submit link
-      </button>
-    </form> `
+    </button>
+</form>
+<div class="ui grid posts">
+    <reddit-article>
+    </reddit-article>
+</div>
+`
 })
 
 export class RedditApp {
